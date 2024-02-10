6b568d293ec80f8db40d4a7558d3acc6b409f1b0
Rana Gül Ayyıldız <132673459+rana42s@users.noreply.github.com>
2024-02-10 09:56:55 +0300

    Add project files.

---
+++ MaskTrackingSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+SelamVer(isim:"Rana");
+SelamVer();
+
+
+static void SelamVer(string isim = "isimisiz")  //SelamVer Metodu
+{
+    Console.WriteLine("Merhaba" + " " + isim);
+}
+
+Person person1 = new Person();
+person1.FirstName = "Rana Gül";
+person1.LastName = "AYYILDIZ";
+person1.DateOfBirthYear = 2003;
+person1.NationalIdentity = 123;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+Console.ReadLine();
